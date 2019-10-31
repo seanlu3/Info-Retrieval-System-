06635bdd48f0e15ee118cea662532cafc2d950d4
Marc29312 <47401977+Marc29312@users.noreply.github.com>
2019-10-31 12:18:22 -0400

    Fixed index out of bounds error
    -Removes blank lines from query.txt (writes correct format to query1.txt)

---
+++ Eval.java
@@ -0,0 +1,127 @@
+import java.io.File;
+import java.io.FileWriter;  
+import java.io.FileInputStream;  
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Eval{
+    private static String queryTextFilePath = "query1.text";
+    private static String qrelsFilePath = "qrels.text";
+    private static String stopwordPath = "stopwords.txt";
+    private static ArrayList<String> stopwordList = new ArrayList<String>();
+    private static Map<String, Integer> query = new TreeMap<String, Integer>();
+
+
+    public static void main(String args[]){
+        createStopwordList(stopwordPath);
+        createTreeMap(queryTextFilePath);    
+        System.out.println(query);    
+    }
+
+    private static void createStopwordList(String path){
+        try {
+			Scanner scanner = new Scanner(new File(path));
+			while (scanner.hasNextLine()) {
+				stopwordList.add(scanner.nextLine());
+			}
+            scanner.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+    private static void createTreeMap(String path){
+    	//This removes the blank lines from query.txt
+    	Scanner file;
+        PrintWriter writer;
+        try {
+
+            file = new Scanner(new File("query.text"));
+            writer = new PrintWriter("query1.text");
+
+            while (file.hasNext()) {
+                String line = file.nextLine();
+                line = line.trim();
+                if (!line.equals("")) {
+                    writer.write(line);
+                    writer.write("\n");
+                }
+            }
+
+            file.close();
+            writer.close();
+
+        } catch (FileNotFoundException ex) {
+            
+        }
+        
+        //
+        String tmp = "";
+        int docId = 1;
+        Boolean stopword = true;
+
+        try{
+            Scanner scanner = new Scanner(new File(path));
+            while (scanner.hasNextLine()){
+                tmp = scanner.nextLine();
+                if (tmp.substring(0,2).toLowerCase().equals(".i")) {
+                    docId = Integer.parseInt(tmp.substring(3));
+                    System.out.println(docId);
+                    tmp = scanner.nextLine();
+                    //System.out.println(tmp);
+                }
+                if (tmp.toLowerCase().equals(".w")) {
+                    tmp = scanner.nextLine();
+                    // stop scanning if next line is .N
+                    while (!tmp.toLowerCase().equals(".n")) {
+                        // remove all chars that are not a-zA-Z
+                    	System.out.println(tmp);
+                        updateTreeMaps(tmp, docId, stopword);
+                        tmp = scanner.nextLine();
+                    }
+                if(tmp == ".I 0"){
+                    break;
+                }
+
+                    
+                }
+            }
+
+            scanner.close();
+
+        } catch(Exception e){
+            System.out.println("End of file reached");
+        }
+    }
+
+
+
+
+    private static void updateTreeMaps(String input, int id, Boolean stopword){
+        String temp;
+        input = input.replaceAll("[^a-zA-Z ]", " ");
+        Scanner termScanner = new Scanner(input);
+        while(termScanner.hasNext()){
+            temp = termScanner.next().toLowerCase();
+            if(stopword){
+                if(!stopwordList.contains(temp)){
+                    query.put(temp,id);
+                }
+            }else {
+                query.put(temp,id);
+            }
+    
+        }
+        termScanner.close();
+    }
+}
