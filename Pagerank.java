ae6a8759731a8fba4a81c7699ce5bbd332791b76
Marc29312 <47401977+Marc29312@users.noreply.github.com>
2019-11-26 13:25:43 -0500

    Added probability value implementation

--- Pagerank.java
+++ Pagerank.java
@@ -22,12 +22,21 @@ public class Pagerank{
     public static void main (String[] args){
     	iterateFile(path);
         //createMatrix(path);
+    	//System.out.println(array[1982][1]);
         normalizeMatrix();
-        //System.out.print(array[1982][1]);
+        //System.out.println(array[1982][1]);
+        probabilityStep();
+        //System.out.println(array[1982][1]);
     }
 
-    	//***This is a variation of your createMatrix code***
-    
+    //
+    //
+    //As of right now array stores the correct normalized matrix
+    //We now need to implement the probability value (can use a similar
+    //function as the normalization function we used)
+    //
+    //
+    //***This is a variation of your createMatrix code***
     private static void iterateFile(String path) {
     	//Variable assignment
     	String tmp = "";
@@ -83,39 +92,6 @@ public class Pagerank{
             e.printStackTrace();
         }
     }
-    
-    /*private static void createMatrix(String path){
-        String tmp = "";
-        int left, middle, right = 0; 
-        try{
-            Scanner scanner = new Scanner (new File(path));
-            while (scanner.hasNextLine()){
-                tmp = scanner.nextLine();
-                if(tmp.toLowerCase().equals(".x")){
-                    //isolating referece numbers
-                    scanner.nextLine();
-                    while(scanner.hasNextInt()){
-                        //isolating three numbers for each line. 
-                        left = scanner.nextInt();
-                        middle = scanner.nextInt();
-                        right = scanner.nextInt();
-                        System.out.println(left + " " + middle + " " + right);
-
-                        //if the middle number is 5, it means that the referencing happens. 
-                        //if left documnet is greater than the right document, it means the left doc has a reference in right doc.  Vise versa. 
-                        if(middle == 5){
-                            if(left > right) array[left][right] = 1;
-                            else if (right > left) array[right][left] = 1;
-                        }
-                    }
-                    scanner.nextLine();
-                }
-            }
-            scanner.close();
-        }catch(Exception e){
-            System.out.println("File end reached");
-        }
-    }*/
 
     //Iterates through array and keeps track of total number of 1's on each
     //row and afterwards will normalize each row
@@ -136,6 +112,20 @@ public class Pagerank{
     				//System.out.println(array[i][x]);
     			}
     		}
+    	}
+    }
+    
+    //Multiplies each non-zero entry by (1-(0.85))
+    private static void probabilityStep() {
+    	for (int i = 1; i < array.length; i++) {
+    		for (int j = 1; j < array.length; j++) {
+    			//checks for all entries in the array above 0 
+    			if(array[i][j]>0) {
+    				//1 - 0.85 = 0.15
+    				array[i][j]=(array[i][j]*0.15);
+    			}
+    		}
+
     	}
     }
 
